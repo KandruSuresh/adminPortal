26cdeb6bfb9fe1f19e39b7331b19c62ab53d1b25
Suresh Kandru <kandru.sureshbabu@gmail.com>
2025-03-03 13:27:10 +0530

    husky changes

--- admin-portal/src/App.tsx
+++ admin-portal/src/App.tsx
@@ -13,7 +13,7 @@ function App() {
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} 
+          <img src={reactLogo}    
         </a>
       </div>
       <h1>Vite + React</h1>
